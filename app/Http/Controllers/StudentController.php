101f01b62179cda498b46c2f3ca705c92c9d9217
Fiorentina Aamara Putri Emile <143419596+applepiren@users.noreply.github.com>
2025-05-08 20:53:37 +0700

    part-4

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -11,9 +11,7 @@ use Illuminate\View\View;
 
 class StudentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index(): View
 {
     $students = Student::all();
@@ -21,17 +19,13 @@ class StudentController extends Controller
 }
 
 
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create(): View
     {
         return view('students.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+   
     public function store(Request $request): RedirectResponse
     {
         $input = $request->all();
@@ -39,35 +33,34 @@ class StudentController extends Controller
         return redirect('students')->with('flash_message', 'Student Addedd!');
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
+   
+    public function show(string $id): View
     {
-        //
+        $students = Student::find($id);
+        return view('students.show')->with('students', $students);
+    }
+    
+    public function edit(string $id): View
+    {
+        $student = Student::find($id);
+        return view('students.edit')->with('students', $student);
+    }
+    
+
+   
+    public function update(Request $request, string $id):RedirectResponse
+    {
+        $student = Student::find($id);
+        $input = $request->all();
+        $student->update($input);
+        return redirect('students')->with('flash_message', 'student Updated!');  
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    
+    public function destroy(string $id):RedirectResponse
     {
-        //
+        Student::destroy($id);
+        return redirect('students')->with('flash_message', 'Student deleted!');  
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+   
 }
