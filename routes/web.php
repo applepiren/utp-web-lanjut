93aa071ce8afdfa82c7fc77961bc890c53a8dcd9
Fiorentina Aamara Putri Emile <143419596+applepiren@users.noreply.github.com>
2025-05-11 13:18:15 +0700

    part-7

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CourseController;
 use App\Models\Course;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BatchController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,3 +26,5 @@ Route::resource('/students', StudentController::class);
 Route::resource('/teachers', TeacherController::class);
 
 Route::resource('/courses', CourseController::class);
+
+Route::resource('/batches', BatchController::class);
